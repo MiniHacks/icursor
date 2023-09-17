23c07418992894a88b60e74ecfbfdb83047b9cf4
minizou <gh.mini.zou@gmail.com>
2023-09-17 05:47:50 -0400

    🧼 cleaning

--- packages/chrome/popup.js
+++ packages/chrome/popup.js
@@ -5,7 +5,7 @@ predictGaze.addEventListener('click', async () => {
     const state = (await chrome.storage.sync.get(["tracking"])).tracking
     console.log(state);
     predictGaze.value = state == 'true' ? 'false' : 'true';
-    predictGaze.textContent = predictGaze.value == 'true' ? "Disable" : "Enable";
+    predictGaze.textContent = predictGaze.value == 'true' ? "Disable Eye Tracking" : "Enable Eye Tracking";
     await chrome.storage.sync.set({ "tracking": predictGaze.value });
 });
 
@@ -23,7 +23,7 @@ window.addEventListener("load", async () => {
     const isTracking = await chrome.storage.sync.get(["tracking"]);
 
     if (isTracking !== null) {
-        predictGaze.textContent = isTracking.tracking == 'true' ? "Disable" : "Enable";
+        predictGaze.textContent = isTracking.tracking == 'true' ? "Disable Eye Tracking" : "Enable Eye Tracking";
         predictGaze.value = isTracking.tracking
     } else {
         await chrome.storage.sync.set({ "tracking": "false" });
