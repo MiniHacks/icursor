e8601a7eab15e3f4b12912451e3ad7e76e6b6bb5
minizou <gh.mini.zou@gmail.com>
2023-09-16 21:06:07 -0400

    📺 injecting

--- packages/chrome/background.js
+++ packages/chrome/background.js
@@ -1,12 +1,29 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'changeColor') {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.scripting.executeScript({
-          target : {tabId : tabs[0].id},
-          files : ["test2.js"]
-        }).then(
-          () => console.log("script injected!")
-        );
-      });
-    }
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target : {tabId : tabs[0].id},
+        files : ["injection.js"]
+      }).then(
+        () => console.log("script injected!")
+      );
+    });
   });
+
+  // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //     chrome.scripting.executeScript({
+  //       target : {tabId : tabs[0].id},
+  //       files : ["./injection/injection.js"]
+  //     }, () => {
+  //       chrome.scripting.executeScript({
+  //         target : {tabId : tabs[0].id},
+  //         args: [message],
+  //         func: (...args) => messageHandler(message)
+  //       })
+  //     }).then(
+  //       () => console.log("script injected!")
+  //     );
+  //   });
+  // });
+
+
--- packages/chrome/injection/injection.js
+++ packages/chrome/injection/injection.js
@@ -1,45 +1,56 @@
 console.log("injection.js loaded");
 
 function calibrate(sender) {
-  chrome.scripting.executeScript({
-    target: { tabId: sender.tab.id },
-    function: () => {
-      const script = document.createElement('script');
-      script.src = chrome.runtime.getURL('webgazer.min.js');
-      document.head.appendChild(script);
+  console.log("calibrate shit")
+  // chrome.scripting.executeScript({
+  //   target: { tabId: sender.tab.id },
+  //   function: () => {
+  //     const script = document.createElement('script');
+  //     script.src = chrome.runtime.getURL('webgazer.min.js');
+  //     document.head.appendChild(script);
 
-      // webgazer.begin() // starting point here
-    },
-  });
+  //     // webgazer.begin() // starting point here
+  //   },
+  // });s
 }
 
-function track(sender) {
-  chrome.scripting.executeScript({
-    target: { tabId: sender.tab.id },
-    function: () => {
-      const script = document.createElement('script');
-      script.src = chrome.runtime.getURL('webgazer.min.js');
-      document.head.appendChild(script);
+function enable(sender) {
+  console.log("enable shit")
+  // chrome.scripting.executeScript({
+  //   target: { tabId: sender.tab.id },
+  //   function: () => {
+  //     const script = document.createElement('script');
+  //     script.src = chrome.runtime.getURL('webgazer.min.js');
+  //     document.head.appendChild(script);
 
-      // webgazer.begin() // starting point here
-    },
-  });
+  //     // webgazer.begin() // starting point here
+  //   },
+  // });
 }
 
+function disable(sender) {
+  console.log("disable shit")
+}
 
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
-  if (msg === "calibrate") {
-    calibrate(sender);
-  }
+// chrome.runtime.onMessage.addListener(function (message) {
+//   console.log("received message: ", message);
+// });
 
-  // need to expand this
-  if (msg == "track") {
-    track(sender);
-  }
+function handleMessage(message)  {
+  console.log("received message: ", message);
+}
 
-  response({ message: "it worked" });
+// const messageHandler = (message) => {
+//   if (message === "calibrate") {
+//     calibrate("");
+//   } else if (message === "enableGaze") {
+//     enable("");
+//   } else if (message === "disableGaze") {
+//     disable("");
+//   }
 
-});
+//   response({ message: "it worked" });
+// };
 
 
 
